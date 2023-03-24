dc94ef8fc5e638b5524d1a800d02a4c936d266e6
kenchrao31 <126147970+kenchrao31@users.noreply.github.com>
2023-03-24 13:39:47 +0530

    Add files via upload

---
+++ accept.java
@@ -0,0 +1,25 @@
+package popups;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class accept {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeOptions option=new ChromeOptions();
+	    option.addArguments("--remote-allow-origins=*");
+	    WebDriver driver=new ChromeDriver(option);
+        driver.manage().window().maximize(); 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
+        driver.get("https://letcode.in/alert");
+        driver.findElement(By.id("accept")).click();
+        Thread.sleep(2000);
+        driver.switchTo().alert().accept();
+
+	}
+
+}
