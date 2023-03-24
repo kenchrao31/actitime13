8ad0a439f46c223c142a42f0c11e6384cbba6d92
kenchrao31 <126147970+kenchrao31@users.noreply.github.com>
2023-03-24 14:20:46 +0530

    Update accept.java

--- accept.java
+++ accept.java
@@ -23,6 +23,9 @@ public class accept {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+		System.out.println("hii");
+		System.out.println("hey");
+		System.out.println("hii");
 
 	}
 
